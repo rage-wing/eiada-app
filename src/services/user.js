ae6efae1c97897b7dc11e553a7de220591fde3ae
Abdelhamied <Abdelhamied403@gmail.com>
2022-08-20 12:49:49 +0200

    sentury setup

--- src/services/user.js
+++ src/services/user.js
@@ -2,6 +2,8 @@ import {api} from './api';
 
 const user = (() => {
   const login = async payload => {
+    console.log(api.defaults.baseURL);
+
     const res = await api.post('user/login', payload);
     return res.data;
   };
